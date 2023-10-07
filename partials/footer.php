a274ac52b0f956669a30e7255c694163a8d9ca41
janahani <Jana2100237@miuegypt.edu.eg>
2023-10-07 14:51:55 +0200

    add partials and change to php

---
+++ partials/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+    <div class="fcontainer">
+        <div class="row">
+
+            <div class="f-col">
+                <h3>Contact Us</h3>
+                <ul>
+                    <li>0123456788</li>
+                    <li><a href="mailto:profitgym.org">contact@profitgym.com</a></li>
+                    <p>We are open daily from 10am — 12am. </p>
+
+                </ul>
+            </div>
+            <div class="f-col">
+                <h3>About Us</h3>
+                <p>ProFit Gym Info
+                </p>
+                <p>Mansoura, Egypt </p>
+
+            </div>
+            <div class="f-col">
+                <h3>Follow us</h3>
+                <div class="social-links">
+
+                    <!-- gets the icons mn font awesome online library -->
+                    <a href="" target="_blank">
+                        <i class="fa fa-facebook  " style="font-size:22px" id="footerfb" ></i>
+                    </a>
+                    <a href="/">
+                        <i class="fa fa-twitter  " style="font-size:22px" id="footertwitter"></i>
+                    </a>
+                    <a href="/">
+                        <i class="fa fa-instagram  " style="font-size:22px" id="footerig"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="f-col" style="font-size: 14px;">
+                <p>© All Rights Reserved</p>
+            </div>
+
+        </div>
+    </div>
+</footer>
---
+++ views/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />                
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />       
+        <link rel="icon" type="image/x-icon" href="images/dfav.png" />
+
+        
+        <link rel="stylesheet" type="text/css" href="../public/css/index.css">
+
+    <!-- to access font awesome cdn icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>                      
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />       
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/x-icon" href="images/dfav.png" />
+  
+
+    <title>ProFit Gym</title>
+
+</head>
+
+<body>
+    <?php include ("../partials/header.php") ?>
+</body>
+
+<script src="/js/index.js">
+
+</script>
+<?php include ('../partials/footer.php') ?>
+
+</html>
