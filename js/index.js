6f3a685f49202d7a6bac8b7da4d1ed4d7469c86a
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-10-07 10:46:25 +0300

    edits

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
 
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
