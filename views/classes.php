054d28034ed7061f6c157a3c672176619f50b1f4
lailanabill <laila2100232@miuegypt.edu.eg>
2023-10-08 03:30:12 +0300

    classes

---
+++ views/classes.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="icon" type="image/x-icon" href="../public/Images/dfav.png" />
+
+    <!-- to access font awesome cdn icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/x-icon" href="../public/Images/dfav.png" />
+
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../public/CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/classes.css">
+
+
+</head>
+
+<body>
+    <!-- include header -->
+    <?php include("../partials/header.php") ?>
+    <!-- add image -->
+    <section id="welcome-image">
+        <img src="../public/Images/bckgrnd.jpg" alt="Home Image" class="main-img">
+        <div class="img-text">
+            <!-- title over image -->
+            <h1>Our Classes</h1>
+            <h2>Home > Classes</h2>
+        </div>
+    </section>
+
+    <div class="container">
+        <h1 class="heading">Fitness Classes For Every Goal</h1>
+        <div class="card-container">
+
+            <div class="card">
+                <img src="../public/Images/yoga.jpg">
+                <h3>Power Yoga Class</h3>
+                <p>Each class starts with a flowing series to warm up the spine and heat up the body, followed by a series of held postures to build strength, stability, and stamina. Class ends with gentle stretches to improve flexibility.</p>
+            </div>
+
+            <div class="card">
+                <img src="../public/Images/yoga.jpg">
+                <h3>Power Yoga Class</h3>
+                <p>Each class starts with a flowing series to warm up the spine and heat up the body, followed by a series of held postures to build strength, stability, and stamina. Class ends with gentle stretches to improve flexibility.</p>
+            </div>
+
+            <div class="card">
+                <img src="../public/Images/yoga.jpg">
+                <h3>Power Yoga Class</h3>
+                <p>Each class starts with a flowing series to warm up the spine and heat up the body, followed by a series of held postures to build strength, stability, and stamina. Class ends with gentle stretches to improve flexibility.</p>
+            </div>
+
+            <div class="card">
+                <img src="../public/Images/yoga.jpg">
+                <h3>Power Yoga Class</h3>
+                <p>Each class starts with a flowing series to warm up the spine and heat up the body, followed by a series of held postures to build strength, stability, and stamina. Class ends with gentle stretches to improve flexibility.</p>
+            </div>
+
+            <div class="card">
+                <img src="../public/Images/yoga.jpg">
+                <h3>Power Yoga Class</h3>
+                <p>Each class starts with a flowing series to warm up the spine and heat up the body, followed by a series of held postures to build strength, stability, and stamina. Class ends with gentle stretches to improve flexibility.</p>
+            </div>
+
+            <div class="card">
+                <img src="../public/Images/yoga.jpg">
+                <h3>Power Yoga Class</h3>
+                <p>Each class starts with a flowing series to warm up the spine and heat up the body, followed by a series of held postures to build strength, stability, and stamina. Class ends with gentle stretches to improve flexibility.</p>
+            </div>
+
+        </div>
+    </div>
+
+    <?php include("../partials/footer.php") ?>
+
+</body>
+
+</html>
