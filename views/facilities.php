c1411a76674fb6e87544ef1d8abdb02c7f0e3913
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-09 18:43:02 +0300

    create the facilites page

--- views/facilities.php
+++ views/facilities.php
@@ -20,7 +20,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="../public/CSS/index.css">
-    
+    <link rel="stylesheet" type="text/css" href="../public/CSS/facilites.css">
 </head>
 <body>
      <!-- navbar -->
@@ -33,10 +33,55 @@
             <h1 class="classes">Facilites</h1>
         </div>
     </section>
+    <div class="container">
     <div class="row">
-        <div class="col-lg-9">
-            <img src="../public/" alt="">
+        <div class="col-lg-4 col-xl-4 facimgs col align-self-center" >
+            <img src="../public/Images/spa.jpeg" alt="" class="imgs" >
+        </div>
+        <div class="col-lg-8 ">
+            <p>Indulge in the ultimate relaxation and wellness experience in our sauna and steam room. Step into the dry heat of the sauna or the soothing, moist environment of the steam room, and let the stress melt away. These luxurious facilities promote muscle relaxation, improve circulation, and provide a serene escape from the demands of daily life</p>
+        </div>
+       <hr>
+        <div class="col-lg-8 ">
+            <p>Our state-of-the-art treadmills offer a versatile and effective way to improve cardiovascular fitness. With adjustable speed and incline settings, you can customize your workout to match your fitness level and goals. Whether you're walking, jogging, or sprinting, our treadmills provide a cushioned surface that reduces impact on your joints, ensuring a comfortable and safe workout experience.</p>
+        </div>
+       
+        <div class="col-lg-4 col-xl-4 facimgs col align-self-center" >
+            <img src="../public/Images/equipments.jpeg" alt="" class="imgs" >
+        </div>
+        <hr>
+        <div class="col-lg-4 col-xl-4 facimgs col align-self-center" >
+            <img src="../public/Images/lockers.jpeg" alt="" class="imgs" >
+        </div>
+        <div class="col-lg-8 ">
+            <p>Our spacious and well-appointed locker rooms provide the perfect space to freshen up and store your belongings. Featuring private changing areas, 
+                showers, and secure lockers, you can transition seamlessly from your workout to the rest of your day. Complete with convenient amenities like hairdryers, toiletries,
+                 and grooming stations, our locker rooms are designed to ensure your comfort and convenience.</p>
+        </div>
+        <hr>
+        <div class="col-lg-8 ">
+            <p>Step into our state-of-the-art Cardio Area, where you'll find an array of cutting-edge equipment designed to elevate 
+                your cardiovascular fitness. Our treadmills, stationary bikes, elliptical trainers, and rowing machines provide the
+                 perfect platform to improve endurance, burn calories, and reach your fitness goals. With spacious surroundings and a vibrant atmosphere,
+                 you'll be motivated to push your limits and achieve new levels of cardiovascular health.</p>
+        </div>
+       
+        <div class="col-lg-4 col-xl-4 facimgs col align-self-center" >
+            <img src="../public/Images/cardio_area.jpeg" alt="" class="imgs" >
+        </div>
+        <hr>
+        <div class="col-lg-4 col-xl-4 facimgs col align-self-center" >
+            <img src="../public/Images/free_weight.webp" alt="" class="imgs" >
+        </div>
+        <div class="col-lg-8 ">
+            <p>Unleash your inner strength in our well-equipped Weightlifting Area. Whether you're a
+                 seasoned lifter or just starting out, our comprehensive selection of free weights,
+                  weight machines, and barbells offers endless opportunities for strength training. 
+                  From classic exercises like bench presses and squats to targeted muscle workouts, you'll 
+                find the tools you need to build lean muscle, increase strength, and sculpt your ideal physique..</p>
         </div>
     </div>
+    </div>
+    <?php include("../partials/footer.php") ?>
 </body>
 </html>
