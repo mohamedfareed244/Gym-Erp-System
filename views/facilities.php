18059d06058faca997dd481982497c3e979c3f98
janahani <Jana2100237@miuegypt.edu.eg>
2023-10-09 19:28:35 +0200

    slight changes

--- views/facilities.php
+++ views/facilities.php
@@ -19,7 +19,9 @@
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="../public/CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/header.css">
+  <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
+  <link rel="stylesheet" type="text/css" href="../public/CSS/memberships.css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/facilites.css">
 </head>
 <body>
