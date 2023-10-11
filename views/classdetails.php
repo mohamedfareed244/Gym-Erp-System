d4f79bd27c972326441c0bcd4caf45ee4513736e
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-10-11 20:58:55 +0300

    class details

--- views/classdetails.php
+++ views/classdetails.php
@@ -9,14 +9,19 @@
     <link rel="stylesheet" type="text/css" href="../public/CSS/header.css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/index.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/classdetails.css">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/3472d45ca0.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js">
     <script src="https://kit.fontawesome.come/a076d05399.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"
+        integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <!--css/icons/boostrap/jquery/fonts/images end-->
 
     <title>ProFit Gym</title>
@@ -27,28 +32,35 @@
     <?php include("../partials/header.php") ?>
     <!-- header image -->
     <section id="welcome-image">
-        <img src="../public/Images/bckgrnd2.jpg" alt="Home Image" class="main-img">
+        <img src="../public/Images/yogaclass.jpg" alt="Home Image" class="main-img">
     </section>
+
+
     <!-- navbar end -->
 
 
-<!-- class details -->
-<section class="class-details">
-    <div class="class-item">
-        <div class="class-image">
-            <img src="../public/Images/Gym-Images.jpg" alt="Class Image">
-        </div>
-        <div class="class-info">
-            <h2 class="class-name">Yoga Class</h2>
-            <p class="class-instructor"><strong>Instructor:</strong> Malouka</p>
-            <p class="class-time"><strong>Time:</strong> 7:00 AM - 8:30 AM</p>
-            <p class="class-date"><strong>Date:</strong> Every Monday and Wednesday</p>
-            <p class="class-participants"><strong>Limit of Participants:</strong> 20</p>
-            <p class="class-level"><strong>Level:</strong> Moderate</p>
+    <section id="class-intro">
+        <div class="class-name-box">
 
+            <h2 class="class-name"> You're Viewing: Yoga Class</h2>
         </div>
-    </div>
-</section>
+    </section>
+
+
+
+    <!-- class details -->
+    <section class="class-details">
+        <div class="class-item">
+            <div class="class-info">
+                <p class="class-instructor"><strong>Instructor:</strong> Malouka</p>
+                <p class="class-time"><strong>Time:</strong> 7:00 AM - 8:30 AM</p>
+                <p class="class-date"><strong>Date:</strong> Every Monday and Wednesday</p>
+                <p class="class-participants"><strong>Limit of Participants:</strong> 20</p>
+                <p class="class-level"><strong>Level:</strong> Moderate</p>
+
+            </div>
+        </div>
+    </section>
 
 
 
