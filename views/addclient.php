6377ccf2743bc529cccb8bc8cb645ab91960062d
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-13 22:33:50 +0300

    start in add client forms

--- views/addclient.php
+++ views/addclient.php
@@ -6,15 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Client | Profit </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/usersidebar.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/adminsidebar.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">  
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-   
-            <?php require("../partials/adminsidebar.php")?>
+<?php require("../partials/adminsidebar.php")?>
+  
+<div id="add-body">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-12" >
+            <label for="name">Name : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="col-lg-4 col-md-12">
+            <label for="name">phone : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="col-lg-4 col-md-12">
+            <label for="name">Email : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <br>
+        <br>
+        <div class="col-lg-4 col-md-12">
+            <label for="name">National id  : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="col-lg-4 col-md-12">
+            <label for="name">Age : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="col-lg-4 col-md-12">
+            <label for="name"> Weight : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <br>
+        <br>
+
+        <div class="col-lg-4 col-md-12">
+            <label for="name"> Height : </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="col-lg-4 col-md-12">
+            <label for="name"> Address : </label>
+            <input type="text" name="name" id="name">
+        </div>
+    </div>
+</div>
+</div>
+           
     
 
     
