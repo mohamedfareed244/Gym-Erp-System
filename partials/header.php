0f6efe76ef14af8b536fcc4e715d0292213886fa
fatimahelsayed <fatimahhatem3@gmail.com>
2023-10-13 18:39:14 +0300

    navbar and login form fixed
    will work on register later on

--- partials/header.php
+++ partials/header.php
@@ -1,59 +1,60 @@
 
-    <!-- Navigation Bar Start -->
-    <header class="gymheader">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top mask-custom shadow-0">
+    <div class="navbar container">
+        <a class="navbar-brand" style="color: rgb(231, 55, 55);" href="../views/index.php">Profit Gym</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto">
 
-        <a id="gymtitle">ProFit Gym</a>
-        <input type="checkbox" id="check">
-        <label for="check" class="gymicons">
-            <span class="material-symbols-outlined" id="menu-icon" >
-                menu
-                </span>
-                <span class="material-symbols-outlined" id="close-icon">
-                    close
-                    </span>
-        </label>
-        <nav class="gymnavbar">
-            <a href="../views/index.php" id="home">HOME</a>
-            <a href="../views/classes.php" id="classes"> CLASSES</a>
-            <a href="../views/memberships.php" id="memberships">MEMBERSHIPS</a>
-
-            <a href="../views/facilities.php" id="facilities">FACILITIES</a>
-            <a href="../views/contactus.php" id="contactus">CONTACT US</a>
-            <a href="../views/login.php"><i class="ri-account-circle-line"></i></a>
-
-
-            
-        </nav>
-    </header>
-    <!-- image starts -->
-
-   
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $(".navigation .navbar .logo-toggle-container .toggle-box").click(function () {
-                $(".navigation .menu").stop().slideToggle();
-            });
-        });
-    </script>
-
+            </ul>
+            <ul class="navbar-nav d-flex"">
+                <li class=" nav-item me-3 me-lg-0">
+                <a class="nav-link" href="../views/index.php">
+                    Home
+                </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="../views/classes.php">
+                        Classes
+                    </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="../views/memberships.php">
+                        Memberships
+                    </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="../views/facilities.php">
+                        Facilites
+                    </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="../views/contactus.php">
+                        Contact us
+                    </a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="../views/login.php">
+                        <i class="ri-account-circle-line"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <script>
-  
-  jQuery(function($){
-
-      var $navbar=$('.header');
-      $(window).scroll(function(event){
-          var $current=$(this).scrollTop();
-
-          if($current >190){
-              $navbar.addClass('navbar-color');
-          }else{
-              $navbar.removeClass('navbar-color');
-          }
-
-      });
-  });
-
+    window.addEventListener("scroll", function() {
+        var navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+            navbar.classList.remove("navbar-dark");
+            navbar.classList.add("navbar-light");
+        } else {
+            navbar.classList.remove("navbar-light");
+            navbar.classList.add("navbar-dark");
+        }
+    });
 </script>
-
-    
+<script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
--- views/facilities.php
+++ views/facilities.php
@@ -1,42 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facilites</title>
+head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!--css/icons/boostrap/jquery/fonts/images start-->
-    <link rel="stylesheet" type="text/css" href="../public/CSS/header.css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/facilites.css">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
-    <!--css/icons/boostrap/jquery/fonts/images end-->
-
-    <!-- bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
+  <!--css/icons/boostrap/jquery/fonts/images start-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="../public/CSS/header.css">
+  <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
+  <link rel="stylesheet" type="text/css" href="../public/CSS/facilites.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">  
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+  <!--css/icons/boostrap/jquery/fonts/images end-->
+  <title>ProFit Gym</title>
 
 </head>
 
 <body>
-    <!-- navbar -->
-    <?php include("../partials/header.php") ?>
-    <!-- add image -->
-    <section id="welcome-image">
-        <img src="../public/Images/bckgrnd2.jpg" alt="Home Image" class="main-img">
-        <div class="img-text">
-            <!-- title over image -->
-            <h1>Facilites</h1>
-        </div>
-    </section>
-    <div class="container">
+  <!-- include header -->
+  <?php include("../partials/header.php") ?>
+  <!-- add image -->
+  <section id="welcome-image">
+    <img src="../public/Images/bckgrnd2.jpg" alt="Home Image" class="main-img">
+    <div class="img-text">
+      <!-- title over image -->
+      <h1>Facilities</h1>
+    </div>
+  </section>
+    <div class="container py-5">
         <div class="row">
             <div class="col-lg-4 col-xl-4 facimgs col align-self-center">
                 <img src="../public/Images/spa.jpeg" alt="" class="imgs">
