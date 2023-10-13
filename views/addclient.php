573b2407d65b643341ba6ff707cf53d4e4656f5d
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-13 20:27:59 +0300

    start in add client page

---
+++ views/addclient.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Client | Profit </title>
+        <!--css/icons/boostrap/jquery/fonts/images start-->
+        <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/usersidebar.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <script src="https://kit.fontawesome.com/3472d45ca0.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.come/a076d05399.js"></script>
+    <!--css/icons/boostrap/jquery/fonts/images end-->
+</head>
+<body>
+    <div class="column">
+        <div class="col-lg-4 col-xl-5 col-sm-4">
+        <?php require("../partials/adminsidebar.php") ?>
+     
+        </div>
+      
+   
+    </div>
+</body>
+</html>
