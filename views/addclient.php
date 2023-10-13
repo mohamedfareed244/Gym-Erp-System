40230c3dfce331c0b92231a0b452f4dbf889b154
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-14 01:51:35 +0300

    fix bug in loading edited css files

--- views/addclient.php
+++ views/addclient.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Client | Profit </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/adminsidebar.css">
+    <link rel="stylesheet"  href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
--- views/editclient.php
+++ views/editclient.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit client | Profit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/adminsidebar.css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/editclient.css">
+    <link rel="stylesheet"  href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">  
@@ -18,7 +18,27 @@
 <div id="add-body">
 <div class="container">
     <div class="row">
+      <form action="" class="row">
+        <div class="col-5">
+        <label for="search">client id : </label>
+            <input type="text" name="search">
+           
+           
+        </div>
+        <div class="col-5">
+        <label for="search1">client phone : </label>
+        <input type="text" name="search1">
+           
+        </div>
+        <div class="col-1">
+            <input type="submit" value="Search" id="add-btn">
+        </div>
+       
+        
+      </form>
+      <hr>
         <form action="" class="row">
+        
         <div class="col-lg-4 col-md-12" >
             <label for="name">Name : </label>
             <input type="text" name="name" id="name">
@@ -58,7 +78,7 @@
             
         </div>
         <div class="col-lg-9 col-md-12">
-        <input type="submit" value="Add client" id="add-btn">
+        <input type="submit" value="Edit" id="add-btn">
             
         </div>
        
