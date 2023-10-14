0a511c3fe63fba84b3d5e624aa13636f87613181
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-14 20:27:19 +0300

    50% of admin details

--- views/profile.php
+++ views/profile.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css?v=<?php echo time(); ?>">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" type="text/css"
@@ -18,8 +18,29 @@
 </head>
 <body>
 <?php require("../partials/adminsidebar.php")?>
-    <div id="tablediv">
+    <div id="add-body">
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-3 col-sm-1">
+    <img src="../public/Images/bckgrnd.jpg" alt="" id="empimg">
+</div>
+<div class="col-5">
+    <h3>Mohamed Fareed</h3>
+    <br>
+    <span class="text-info">Mohamed@gmail.com </span><span>- Administrator</span>
+    <br>
+    <br>
+    <label for="photo">Change image </label>
+    <input type="file" name="photo" id="imgfile">
+</div>
+            </div>
 
+        </div>
+<hr>
+<h3>Account : </h3>
+<form action="" class="row">
+    
+</form>
     </div>
 </body>
 </html>
