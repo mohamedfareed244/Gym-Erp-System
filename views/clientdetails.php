4605e2ce525e3f917483f37e9c23dd71a5516836
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-14 13:23:25 +0300

    fix the bug of css updated files

--- views/clientdetails.php
+++ views/clientdetails.php
@@ -7,7 +7,7 @@
     <title>Client Details | Profit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet"  href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient?v=<?php echo time(); ?>.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">  
@@ -16,7 +16,158 @@
 <body>
     <?php require("../partials/adminsidebar.php")?>
     <div id="add-body">
-        <p>abc</p>
+        <div class="container">
+        <form action="" class="row">
+        <div class="col-5">
+        <label for="search">client id : </label>
+            <input type="text" name="search">
+           
+           
+        </div>
+        <div class="col-5">
+        <label for="search1">client phone : </label>
+        <input type="text" name="search1">
+           
+        </div>
+        <div class="col-2">
+            <input type="submit" value="Search" id="add-btn">
+        </div>
+       
+      </form>
+      <br>
+      <hr>
+      <h2>Memberships</h2>
+      <div id="tablediv">
+      <table class="table overflow-auto mh-10">
+        <thead>
+        <tr>
+      <th scope="col">Package Name</th>
+      <th scope="col">Start Date </th>
+      <th scope="col">End Date</th>
+      <th scope="col">Visits </th>
+      <th scope="col">Status </th>
+      <th scope="col">Freezes </th>
+      <th scope="col">Action </th>
+      <th scope="col">Sales</th>
+    </tr>
+        </thead>
+<tbody>
+<tr>
+      <th scope="row">2 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-info">freezed</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    <tr>
+      <th scope="row">6 Months offer</th>
+      <td>02-09-2023</td>
+      <td>02-09-2023</td>
+      <td>30</td>
+      <td class="bg-danger">expired</td>
+      <td>2 Weeks</td>
+      <td><button>Freeze</button></td>
+      <td>Mohamed fareed</td>
+    </tr>
+    
+</tbody>
+       </table>
+      </div>
+      
+        </div>
     </div>
 </body>
 </html>
