820873078ae274c66236ec12727a1b6127b1d77e
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-14 12:54:07 +0300

    20% of client details file and the nav links to client pages

--- partials/adminsidebar.php
+++ partials/adminsidebar.php
@@ -27,8 +27,8 @@
                 <ul class="sub-menu">
                     <li><a  class="link-name" href="#">Clients</a></li>
                     <li><a href="../views/addclient.php">Add</a></li>
-                    <li><a href="../views/client.php">Edit</a></li>
-                    <li><a href="#">Details</a></li>
+                    <li><a href="../views/editclient.php">Edit</a></li>
+                    <li><a href="../views/clientdetails.php">Details</a></li>
                 </ul>
             </li>
        
