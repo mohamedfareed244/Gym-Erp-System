11926c23fab14cfab62f655b6177f7bca813f0af
janahani <Jana2100237@miuegypt.edu.eg>
2023-10-16 19:58:23 +0200

    bookptpackage php+css

---
+++ views/bookptpackage.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--css/icons/boostrap/jquery/fonts/images start-->
+    <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/usersidebar.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/userprofile.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/bookptpackage.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <script src="https://kit.fontawesome.com/3472d45ca0.js" crossorigin="anonymous"></script>
+    <!--css/icons/boostrap/jquery/fonts/images end-->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+
+</head>
+
+<body>
+    <!-- usersidebar start -->
+    <?php include("../partials/usersidebar.php") ?>
+    <div class="profile">
+
+        <div class="greeting">
+        <p class="class">Personal Trainer:</p>
+                <div class="class-title">Sara Maged</div>
+        </div>
+
+        <div class="reminders">
+            <div class="reminder">
+                <div class="class-title">1 Months, 15 Sessions</div>
+
+
+                <div class="rem-info">
+                    <p>Working Days:</p>
+                    <p class="actual-rem"> Sat, Mon, Wed, Fri</p>
+
+                    <p>Working Hours:</p>
+                    <p class="actual-rem"> 10 AM - 5 PM</p>
+
+                    <p>PT Fees:</p>
+                    <p class="actual-rem"> 1500 L.E.</p>
+                </div>
+                <button id="trainer-booking">Book Now</button>
+            </div>
+
+
+
+            <div class="reminder">
+                <div class="class-title">2 Months, 30 Sessions</div>
+
+
+                <div class="rem-info">
+                    <p>Working Days:</p>
+                    <p class="actual-rem"> Sat, Mon, Wed, Fri</p>
+
+                    <p>Working Hours:</p>
+                    <p class="actual-rem"> 10 AM - 5 PM</p>
+
+                    <p>PT Fees:</p>
+                    <p class="actual-rem"> 3500 L.E.</p>
+                </div>
+                <button id="trainer-booking">Book Now</button>
+            </div>
+
+
+
+
+        
+            <div class="reminder">
+                <div class="class-title">4 Months, 40 Sessions</div>
+
+
+                <div class="rem-info">
+                    <p>Working Days:</p>
+                    <p class="actual-rem"> Sat, Mon, Wed, Fri</p>
+
+                    <p>Working Hours:</p>
+                    <p class="actual-rem"> 10 AM - 5 PM</p>
+
+                    <p>PT Fees:</p>
+                    <p class="actual-rem"> 4500 L.E.</p>
+                </div>
+                <button id="trainer-booking">Book Now</button>
+            </div>
+
+</div>
+
+    </div>
+
+
+</body>
+<?php include("../partials/footer.php") ?>
+
+<script src="../public/js/personaltrainer.js"></script>
+
+</html>
