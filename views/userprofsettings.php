74fc9c9ae65d48b987b699c479f9897e6dac3bad
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-10-20 00:50:35 +0300

    user profile settings in the making

---
+++ views/userprofsettings.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--css/icons/boostrap/jquery/fonts/images start-->
+    <link rel="stylesheet" type="text/css" href="../public/CSS/footer.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/usersidebar.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/userprofile.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/userprofsettings.css">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <script src="https://kit.fontawesome.com/3472d45ca0.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.come/a076d05399.js"></script>
+    <!--css/icons/boostrap/jquery/fonts/images end-->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+
+</head>
+
+<body>
+    <!-- usersidebar start -->
+    <?php include("../partials/usersidebar.php") ?>
+
+
+    <div class="profile">
+
+        <div class="greeting">
+            <p class="hello-fz"><i class="fas fa-cog"></i> Profile Settings</p>
+        </div>
+
+        <div class="reminders">
+        <div class="profile-settings">
+                <h2>Edit Profile Settings</h2>
+                <form id="profile-settings-form">
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username" placeholder="User's Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="user@gmail.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">New Password:</label>
+                        <input type="password" id="password" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm-password">Confirm Password:</label>
+                        <input type="password" id="confirm-password" name="confirm-password">
+                    </div>
+                    <button id="update-profile-button" type="submit">Update Profile</button>
+                </form>
+            </div>
+</div>
+</div>
+
+
+</body>
+<?php include("../partials/footer.php") ?>
+
+
+<script src="../public/js/index.js"></script>
+
+
+
+</html>
