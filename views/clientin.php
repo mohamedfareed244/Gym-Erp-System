2bfffc0c4da36a1ac29d468abd7a7dbda14bf877
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-21 14:27:25 +0300

    some stat boxes

--- views/clientin.php
+++ views/clientin.php
@@ -7,7 +7,7 @@
     <title>Check in </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
@@ -17,6 +17,23 @@
 <?php require("../partials/adminsidebar.php") ?>
 <div id="add-body">
     <div class="container">
+    <div class="row">
+       <div class="col-3 divstat" >
+        <div class="row">
+            <div class="col-3">
+            <i class="fa fa-circle" style="color:green;margin-top:60px;"></i>
+            </div>
+            <div class="col-9"><h2 style="margin-top:50px;">130/200</h2>
+        <p><b>Estimated clients in the gym</b>  </p>
+        </div>
+        </div>
+
+
+       </div>
+        </div>
+        <br>
+        <br>
+
     <form action="" class="row">
                 <div  class="col-5">
                     <label for="search">Client ID: </label>
@@ -36,11 +53,7 @@
             </form>
             <br>
             <hr>
-        <div class="row">
-       <div class="col-4 divstat" >
-
-       </div>
-        </div>
+      
     </div>
 </div>
 </body>
