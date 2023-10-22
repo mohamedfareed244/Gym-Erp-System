cdda14ca3ad100295de1d6a6e2c0e32a1bd0fc0a
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-10-22 17:58:49 +0300

    sales report

--- views/salesreport.php
+++ views/salesreport.php
@@ -9,35 +9,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/salesreport.css">
+
+    
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-    <style>
-        .date-container {
-            display: flex;
-        }
 
-        .date-input {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin: 5px;
-            border-radius: 5px;
-            font-size: 15px;
-            text-transform: uppercase;
-            background-color: transparent;
-            letter-spacing: 1px
-
-            
-        }
-
-        .date-label {
-            font-size: 18px;
-            margin: 5px;
-            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-        }
-    </style>
 </head>
 
 <body>
@@ -60,8 +40,16 @@
         <label class="date-label">To:</label>
         <input type="date" class="date-input">
     </div>
+
+
+
        </div>
         </div>
+        <div class="center-button-container">
+
+        <button class="generate-report-button">Generate Report</button>
+
+    </div>
     </div>
 
 </body>
