0dbf26d8c0c9f2486d63e79779d7706b946fda40
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-10-22 23:22:04 +0300

    myptclients

--- views/myptclients.php
+++ views/myptclients.php
@@ -7,13 +7,40 @@
     <title>My Clients </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/addclientq.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient1.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-    
+<?php require("../partials/adminsidebar.php") ?>
+<div id="add-body">
+<div id="tablediv">
+    <table class="table">
+    <thead>
+                        <tr>
+                          <th scope="col">client ID</th>
+                            <th scope="col">client Name</th>
+                            <th scope="col">client phone</th>
+                            <th scope="col">pt package name</th>
+                            <th scope="col">sessions</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <th>Mohamed</th>
+                            <th>0121084789</th>
+                            <th>20-sessions</th>
+                            <th>3 From 20</th>
+                            <th><button id="add-btn">Attend</button></th>
+                        </tr>
+                    </tbody>
+                    
+    </table>
+</div>
+</div>
 </body>
 </html>
