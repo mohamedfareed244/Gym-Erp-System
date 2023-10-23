4cfae159850308d9e75e0c59ece69c2e3ca3e677
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-10-23 22:22:16 +0300

    backend for adding clients

--- views/addclient.php
+++ views/addclient.php
@@ -1,78 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Client | Profit </title>
+    <title>Add Client | Profit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-
 <body>
     <?php require("../partials/adminsidebar.php") ?>
-
     <div id="add-body" class="addbody">
         <div class="container">
             <div class="row">
-            <div class="col-12">
-                    <h4>Add client </h4>
+                <div class="col-12">
+                    <h4>Add client</h4>
                 </div>
                 <hr>
-                <form action="" class="row">
+                <form method="POST" >
+                <!-- action="addclientbackend.php" -->
                     <div class="col-lg-4 col-md-12">
-                        <label for="name">Name : </label>
+                        <label for="fname">First Name: </label>
                     </div>
-                    <input type="text" name="name" id="name">
+                    <input type="text" name="fname" id="fname">
+                    
                     <div class="col-lg-4 col-md-12">
-                        <label for="phone">phone : </label>
+                        <label for="lname">Last Name: </label>
+                    </div>
+                    <input type="text" name="lname" id="lname">
+                    
+                    <div class="col-lg-4 col-md-12">
+                        <label for="age">Age: </label>
+                    </div>
+                    <input type="text" name="age" id="age">
+                    
+                    <div class="col-lg-4 col-md-12">
+                        <label for="phone">Phone: </label>
                     </div>
                     <input type="text" name="phone" id="phone">
+                    
                     <div class="col-lg-4 col-md-12">
-                        <label for="email">Email : </label>
+                        <label for="gender">Gender: </label>
                     </div>
-                    <input type="email" name="email" id="email">
+                    <input type="text" name="gender" id="gender">
+                    
                     <div class="col-lg-4 col-md-12">
-                        <label for="nationalid">National id : </label>
-                    </div>
-                    <input type="text" name="nationalid" id="nationalid">
-                    <div class="col-lg-4 col-md-12">
-                        <label for="age">Age : </label>
-                    </div>
-                    <input type="number" name="age" id="age">
-                    <div class="col-lg-4 col-md-12">
-                        <label for="weight"> Weight : </label>
+                        <label for="weight">Weight: </label>
                     </div>
                     <input type="number" name="weight" id="weight">
+                    
                     <div class="col-lg-4 col-md-12">
-                        <label for="height"> Height : </label>
+                        <label for="height">Height: </label>
                     </div>
                     <input type="number" name="height" id="height">
+                    
                     <div class="col-lg-4 col-md-12">
-                        <label for="address"> Address : </label>
+                        <label for="email">Email: </label>
                     </div>
-                    <textarea name="address" id="" cols="80" rows="1"></textarea>
-                    <div class="col-lg-9 col-md-12">
+                    <input type="email" name="email" id="email">
                     <br>
+                    <div class="col-lg-9 col-md-12">
                         <input type="submit" value="Add client" id="add-btn">
-
                     </div>
-
                 </form>
-
-
                 <div class="col-12">
-                    <span>New Client id : <b>12345</b></span>
+                    <span>New Client id: <b>12345</b></span>
                 </div>
             </div>
         </div>
     </div>
+
+    <?php
+    include_once "../includes/dbh.inc.php";
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $age = (int) $_POST['age'];
+        $gender = $_POST['gender'];
+        $weight = (float) $_POST['weight'];
+        $height = (int) $_POST['height'];
+        $email = $_POST['email'];
+
+        $sql = "INSERT INTO clients (FirstName, LastName, Age, Gender, Weight, Height, Email)
+            VALUES ('$fname', '$lname', '$age', '$gender', $weight, $height, '$email')";
+
+        if ($conn->query($sql) === true) {
+            echo "Client added successfully.";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
+    ?>
 
 </body>
 
