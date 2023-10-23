8f919d73100da7c472c6616f7f9f9db9fdb8511f
janahani <Jana2100237@miuegypt.edu.eg>
2023-10-23 14:52:44 +0200

    not complete signup

--- views/signup_validation.php
+++ views/signup_validation.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once "includes/dbh.inc.php";
+
 $fnameErr = $lnameErr = $ageErr = $genderErr = $emailErr = $passwordErr = ""; // Initialize error variables
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -46,26 +48,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $passwordErr = "Password must be at least 6 characters long";
     }
 
-    // Prepare the response data
-    $response = array(
-        "success" => empty($fnameErr) && empty($lnameErr) && empty($ageErr) && empty($genderErr) && empty($emailErr) && empty($passwordErr),
-        "errors" => array(
-            "fname" => $fnameErr,
-            "lname" => $lnameErr,
-            "age" => $ageErr,
-            "gender" => $genderErr,
-            "email" => $emailErr,
-            "password" => $passwordErr
-        )
-    );
+ // Check if there are no validation errors
+ if (empty($fnameErr) && empty($lnameErr) && empty($ageErr) && empty($genderErr) && empty($emailErr) && empty($passwordErr)) {
 
+    // Validation successful, save data to the database
+    $Fname = htmlspecialchars($_POST["fname"]);
+    $Lname = htmlspecialchars($_POST["lname"]);
+    $Age = htmlspecialchars($_POST["age"]);
+    $Gender = htmlspecialchars($_POST["gender"]);
+    $Email = htmlspecialchars($_POST["email"]);
+    $Password = htmlspecialchars($_POST["password"]);
 
-    if($response=="success"){
-
+    // Insert data into the database
+    if (!empty($_POST["weight"]) && !empty($_POST["height"])) {
+        $Weight = htmlspecialchars($_POST["weight"]);
+        $Height = htmlspecialchars($_POST["height"]);
+        $sql = "insert into client(FirstName,LastName,Age,Gender,Weight,Height,Email,Password) 
+        values('$Fname','$Lname','$Age','$Gender','$Weight','$Height','$Email','$Password')";
+    } else if (!empty($_POST["weight"]) && empty($_POST["height"])) {
+        $Weight = htmlspecialchars($_POST["weight"]);
+        $sql = "insert into client(FirstName,LastName,Age,Gender,Weight,Email,Password) 
+        values('$Fname','$Lname','$Age','$Gender','$Weight','$Email','$Password')";
+    } else if (empty($_POST["weight"]) && !empty($_POST["height"])) {
+        $Height = htmlspecialchars($_POST["height"]);
+        $sql = "insert into client(FirstName,LastName,Age,Gender,Height,Email,Password) 
+        values('$Fname','$Lname','$Age','$Gender','$Height','$Email','$Password')";
     }
-    // Send the response as JSON
-    echo json_encode($response);
 
+    if ($conn->query($sql) === TRUE) {
+        // Data inserted successfully
+        $insertion_success = true;
+    } else {
+        // Error occurred while inserting data
+        $insertion_success = false;
+    }
 
+    // Close the database connection
+    $conn->close();
+} else {
+    // Validation failed
+    $insertion_success = false;
+}
+
+// Prepare the response data
+$response = array(
+    "success" => empty($fnameErr) && empty($lnameErr) && empty($ageErr) && empty($genderErr) && empty($emailErr) && empty($passwordErr),
+    "insertion_success" => isset($insertion_success) ? $insertion_success : false,
+    "errors" => array(
+        "fname" => $fnameErr,
+        "lname" => $lnameErr,
+        "age" => $ageErr,
+        "gender" => $genderErr,
+        "email" => $emailErr,
+        "password" => $passwordErr
+    )
+);
+
+// Send the response as JSON
+echo json_encode($response);
 }
 ?>
