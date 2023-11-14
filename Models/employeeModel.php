3e80003f9ee90896f6874e199dd38c67abc6893d
fatimahelsayed <fatimahhatem3@gmail.com>
2023-11-14 20:58:00 +0200

    update/delete employee

---
+++ Models/employeeModel.php
@@ -0,0 +1,71 @@
+<?php
+
+session_start();
+
+include_once "../includes/dbh.inc.php";
+
+class Employee
+{
+    public $ID;
+    public $Name;
+    public $Email;
+    public $Password;
+    public $Salary;
+    public $JobTitle;
+    public $Address;
+    public $PhoneNumber;
+
+    public function addEmployee($employee)
+    {
+        global $conn;
+
+        $name = $employee->Name;
+        $Sal = $employee->Salary;
+        $address = $employee->Address;
+        $phoneNumber = $employee->PhoneNumber;
+        $jobTitle = $employee->JobTitle;
+        $Email = $employee->Email;
+        $Password = $employee->Password;
+
+        $sql = "INSERT INTO employee (Name, Salary, Address, PhoneNumber, JobTitle, Email, Password) 
+                VALUES ('$name', '$Sal', '$address', '$phoneNumber', '$jobTitle','$Email', '$Password')";
+        return mysqli_query($conn, $sql);
+    }
+    public function updateEmployee($employee)
+    {
+        global $conn;
+
+        $name = $employee->Name;
+        $Sal = $employee->Salary;
+        $address = $employee->Address;
+        $phoneNumber = $employee->PhoneNumber;
+        $jobTitle = $employee->JobTitle;
+        $Email = $employee->Email;
+        $Password = $employee->Password;
+
+        $employee_id = $_SESSION['ID'];
+
+        if (!empty($Password)) {
+            $hashedPassword = password_hash($Password, PASSWORD_DEFAULT);
+            $sql = "UPDATE employee SET Name='$name', Email='$Email', Password='$hashedPassword', Salary='$Sal', PhoneNumber='$phoneNumber', Address='$address', 
+            JobTitle='$jobTitle'
+                    WHERE ID = $employee_id";
+            return $conn->query($sql);
+        } else {
+            // Update with the new password
+            $sql = $sql = "UPDATE employee SET Name='$name', Email='$Email', Salary='$Sal', PhoneNumber='$phoneNumber', Address='$address', 
+            JobTitle='$jobTitle'
+                    WHERE ID = $employee_id";
+            return $conn->query($sql);
+        }
+    }
+
+    public function deleteEmployee()
+    {
+        global $conn;
+
+        $sql = "DELETE from employee where ID =" . $_SESSION['ID'];
+
+        return mysqli_query($conn, $sql);
+    }
+}
