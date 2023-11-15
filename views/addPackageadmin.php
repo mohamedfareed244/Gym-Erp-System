5a0cb9a4c74d186c9ba6fb02c94b35ad0b4e40af
janahani <Jana2100237@miuegypt.edu.eg>
2023-11-15 19:22:50 +0200

    form addPackage

--- views/addPackageadmin.php
+++ views/addPackageadmin.php
@@ -6,25 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Package | Admin </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
     <link rel="stylesheet" type="text/css" href="../public/CSS/addpackage.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <style>
-              .coaches-title{
-            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-  font-size: 27px;
-  text-transform: uppercase;
-  padding-top: 20px;
-  color: rgb(36, 34, 34);
-  /* padding-left: 40px; */
-  padding-bottom: 30px;
-        }
+.coaches-title {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 27px;
+    text-transform: uppercase;
+    padding-top: 20px;
+    color: rgb(36, 34, 34);
+    /* padding-left: 40px; */
+    padding-bottom: 30px;
+}
+
+.visits-container {
+    font-family: Arial, sans-serif;
+    margin: 20px;
+}
+
+.visits-title {
+    font-size: 18px;
+    margin-bottom: 10px;
+}
+
+.radio-buttons {
+    display: flex;
+}
+
+.hidden {
+    display: none;
+}
 </style>
 
 <body>
@@ -38,10 +59,23 @@
                 </div>
                 <hr>
                 <form action="" class="row">
-                <div class="col-lg-4 col-md-12">
-                        <label for="name">Package name : </label>
+                    <div class="visits-container">
+                        <div class="visits-title">Visits:</div>
+                        <div class="radio-buttons">
+                            <input type="radio" id="limited" name="visits" value="limited" class="radio-btn"
+                                onclick="showLimitField()">
+                            <label for="limited" id="limited">Limited</label>
+
+                            <input type="radio" id="unlimited" name="visits" value="unlimited" class="radio-btn"
+                                onclick="hideLimitField()">
+                            <label for="unlimited" id="unlimited">Unlimited</label>
+                        </div>
+
+                        <div id="limitField" class="hidden">
+                            <label for="limitDays">Limit in days:</label>
+                            <input type="text" id="limitDays" name="limitDays">
+                        </div>
                     </div>
-                    <input type="text" name="name" id="invi">
                     <div class="col-lg-4 col-md-12">
                         <label for="name">Number of Months : </label>
                     </div>
@@ -67,7 +101,18 @@
         </div>
     </div>
 
+    <script>
+    function showLimitField() {
+        var limitField = document.getElementById("limitField");
+        limitField.style.display = "block";
 
+    }
+
+    function hideLimitField() {
+        var limitField = document.getElementById("limitField");
+        limitField.style.display = "none";
+    }
+    </script>
 
 </body>
 
