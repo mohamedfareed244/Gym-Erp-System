c9b4bb2fe2e48f7dd525be61493c66980df03608
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-11-16 23:41:52 +0200

    Update CoachesModel.php

--- Models/CoachesModel.php
+++ Models/CoachesModel.php
@@ -17,40 +17,79 @@ class Coach extends Employee
 
 
 
-    public function getAssignedClasses(){
-    global $conn;
+    public function getAssignedClasses($employee)
+    {
+        global $conn;
 
-    if (empty($this->ID)) {
-        return [];
-    }
-
-    $coachID = $this->ID;
-
-    $sql = "SELECT c.* FROM classes c
-            INNER JOIN employee e ON c.Coach = e.ID
-            WHERE e.ID = $coachID AND (e.JobTitle = 'Coach' OR e.JobTitle = 'Fitness Manager')";
-
-    $result = $conn->query($sql);
-
-    $assignedClasses = array();
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $coachclassdetails = new Coach(); // to store class details
-            $coachclassdetails->ID = $row['ID'];
-            $coachclassdetails->Name = $row['Name'];
-            $coachclassdetails->Date = $row['Date'];
-            $coachclassdetails->StartTime = $row['StartTime'];
-            $coachclassdetails->EndTime = $row['EndTime'];
-            $coachclassdetails->Price = $row['Price'];
-            $coachclassdetails->Coach = $row['Coach'];
-
-            $assignedClasses[] = $coachclassdetails;
+        if (empty($this->ID)) {
+            return [];
         }
+
+        // $coachID = $this->ID;
+        $employee_id = $_SESSION['ID'];
+
+        $sql = "SELECT c.* FROM classes c
+            INNER JOIN employee e ON c.Coach = e.ID
+            WHERE e.ID = $employee_id AND (e.JobTitle = 'Coach' OR e.JobTitle = 'Fitness Manager')";
+
+        $result = $conn->query($sql);
+
+        $assignedClasses = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $coachclassdetails = new Coach(); // to store class details
+                $coachclassdetails->ID = $row['ID'];
+                $coachclassdetails->Name = $row['Name'];
+                $coachclassdetails->Date = $row['Date'];
+                $coachclassdetails->StartTime = $row['StartTime'];
+                $coachclassdetails->EndTime = $row['EndTime'];
+                $coachclassdetails->Price = $row['Price'];
+                $coachclassdetails->Coach = $row['Coach'];
+
+                $assignedClasses[] = $coachclassdetails;
+            }
+        }
+
+        return $assignedClasses;
     }
 
-    return $assignedClasses;
+    public function getPTSessions($employee)
+    {
+        global $conn;
+
+        if (empty($this->ID)) {
+            return [];
+        }
+
+        // $coachID = $this->ID;
+        $employee_id = $_SESSION['ID'];
+
+
+        $sql = "SELECT pts.* FROM pt_sessions pts
+            INNER JOIN employee e ON pts.CoachID = e.ID
+            WHERE e.ID = $employee_id AND (e.JobTitle = 'Coach' OR e.JobTitle = 'Fitness Manager')";
+
+        $result = $conn->query($sql);
+
+        $ptSessions = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $ptSessionDetails = new Coach(); // Create an instance to store PT session details
+                $ptSessionDetails->ClientID = $row['ClientID'];
+                $ptSessionDetails->CoachID = $row['CoachID'];
+                $ptSessionDetails->PrivateTrainingPackageID = $row['PrivateTrainingPackageID'];
+                $ptSessionDetails->SessionsCount = $row['SessionsCount'];
+
+                $ptSessions[] = $ptSessionDetails;
+            }
+        }
+
+        return $ptSessions;
+    }
+
+
 }
 
-    
-}
+?>
