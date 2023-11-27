48abfc46cb4d356772bdbd16a3f8bb8c265f1208
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-11-27 20:20:54 +0200

    Update ptMembershipsModel.php

--- Models/ptMembershipsModel.php
+++ Models/ptMembershipsModel.php
@@ -2,6 +2,32 @@
 
 class ptMemberships{
 
+    private $ClientID;
+    private	$CoachID;
+    private	$PrivateTrainingPackageID;
+    public	$SessionsCount;
+    private	$isActivated;	 
+
+
+    
+    public function activatePtMembership($PrivateTrainingPackageID)
+    {
+        global $conn;
+
+        $sql = "UPDATE `private training membership` SET isActivated='Activated' WHERE ID='$PrivateTrainingPackageID'";
+
+        return $conn->query($sql);
+    }
+
+
+    public function deactivatePtMembership($PrivateTrainingPackageID)
+    {
+        global $conn;
+
+        $sql = "UPDATE `private training membership` SET isActivated='Deactivated' WHERE ID='$PrivateTrainingPackageID'";
+
+        return $conn->query($sql);
+    }
 }
 
 ?>
