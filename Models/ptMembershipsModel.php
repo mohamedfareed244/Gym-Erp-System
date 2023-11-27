7cbd2175e8ff8ef6af08a421b89845fafed35b90
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-11-27 21:08:07 +0200

    pt paackages client side

--- Models/ptMembershipsModel.php
+++ Models/ptMembershipsModel.php
@@ -1,6 +1,10 @@
 <?php
 include_once "../includes/dbh.inc.php";
 include_once "ptPackageModel.php";
+include_once "ClientModel.php";
+include_once "CoachesModel.php";
+
+
 
 class ptMemberships{
 
@@ -52,6 +56,43 @@ class ptMemberships{
         $sql = "UPDATE `private training membership` SET isActivated='Deactivated' WHERE ID='$PrivateTrainingPackageID'";
 
         return $conn->query($sql);
+    }
+
+    public static function createPtMembership($clientId, $ptpackageID)
+    {
+        global $conn;
+        $isActivated = "Activated";
+        $client=new Client();
+        $package = new ptPackages();
+        $findClient = $client->checkClient($clientId);
+        $findPackage = $package->checkPtPackage($ptpackageID);
+        // if ($findClient && $findPackage) {
+        //     $Package = $package->getPtPackage($ptpackageID);
+        //     $sql = "INSERT INTO `private training membership` (ClientID, CoachID, PrivateTrainingPackageID, SessionsCount, isActivated)
+        //     -- VALUES ('$clientId', '$','$getisActivated()')";
+        //     return mysqli_query($conn, $sql);
+        // }
+        // return false;
+    }
+
+    function getCoachNames($ptMemberships, $coaches) {
+        $coachNames = array();
+    
+        foreach ($ptMemberships as $ptMembership) {
+            $clientId = $ptMembership['ClientID'];
+            $coachId = $ptMembership['CoachID'];
+    
+            $coach = array_filter($coaches, function ($c) use ($coachId) {
+                return $c['ID'] == $coachId;
+            });
+    
+            if (!empty($coach)) {
+                $coach = reset($coach);
+                $coachNames[$clientId] = $coach['Name'];
+            }
+        }
+    
+        return $coachNames;
     }
 }
 
