08f85401c2fc025ea487d118dc77e2ca4b827d2a
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-12-13 17:02:58 +0200

    self reference menu done

--- views/partials/header.php
+++ views/partials/header.php
@@ -12,10 +12,12 @@ $menuHtml = $menuController->displayMenu();
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto">
+            
+            <ul class="navbar-nav ms-auto">
                 <?php echo $menuHtml; ?>
 
-                <!-- stop here -->
+
+                <!-- log in icon and page are separated -->
                 <li class="nav-item me-3 me-lg-0">
                     <a class="nav-link" href="../views/login.php">
                         <i class="ri-account-circle-line"></i>
@@ -25,9 +27,21 @@ $menuHtml = $menuController->displayMenu();
         </div>
     </div>
 </nav>
+
+<!-- Your script and jQuery/bootstrap scripts remain unchanged -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-w5pCm1vxiUcLvNzHZmsCjOWzIbY4hOaQ50zGCA2N0cE=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
+    window.addEventListener("scroll", function() {
+        var navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+            navbar.classList.remove("navbar-dark");
+            navbar.classList.add("navbar-light");
+        } else {
+            navbar.classList.remove("navbar-light");
+            navbar.classList.add("navbar-dark");
+        }
+    });
     document.addEventListener('DOMContentLoaded', function () {
         var dropdowns = document.querySelectorAll('.navbar-nav .dropdown');
 
