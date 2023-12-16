d4257189e31f3b17dd353ff8cf6ab4e5ba53aff0
janahani <Jana2100237@miuegypt.edu.eg>
2023-12-16 13:51:55 +0200

    my pt clients done

--- Models/CoachesModel.php
+++ Models/CoachesModel.php
@@ -71,6 +71,38 @@ class Coach extends Employee
         return $result;
     }
 
+    public function getptMembershipsForCoach($coachID)
+    {
+        $isActivated = "Activated";
+
+        $sql = "SELECT client.FirstName AS clientFirstName , client.LastName AS clientLastName, `private training package`.Name AS ptPackageName, 
+        `private training package`.NumOfSessions, `private training package`.Price, `private training membership`.SessionsCount, `private training membership`.ID
+        FROM `private training membership`
+        INNER JOIN `private training package` ON `private training membership`.PrivateTrainingPackageID = `private training package`.ID 
+        INNER JOIN client ON `private training membership`.ClientID = client.ID
+        INNER JOIN employee ON `private training membership`.CoachID = employee.ID
+        WHERE `private training membership`.isActivated = '$isActivated' AND employee.ID='$coachID'";
+
+        $result = $this->db->query($sql);
+
+        $results = array();
+
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $results[] = array(
+                    'ID' => $row['ID'],
+                    'clientFirstName' => $row['clientFirstName'],
+                    'clientLastName' => $row['clientLastName'],
+                    'ptPackageName' => $row['ptPackageName'],
+                    'NumOfSessions' => $row['NumOfSessions'],
+                    'Price' => $row['Price'],
+                    'SessionsCount' => $row['SessionsCount'],
+                );
+            }
+            return $results;
+        }
+    }
+
 
 }
 ?>
--- views/myptclients.php
+++ views/myptclients.php
@@ -1,46 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Clients </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--css/icons/boostrap/jquery/fonts/images start-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/CSS/adminsidebar.css?v=<?php echo time(); ?>" type="text/css">
-    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient1.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="../public/CSS/addclient.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!--css/icons/boostrap/jquery/fonts/images start-->
+
+    <title>Admin Dashboard</title>
+
 </head>
+
 <body>
-<?php require("partials/adminsidebar.php") ?>  
-<div id="add-body">
-<div id="tablediv">
-    <table class="table">
-    <thead>
+    <?php session_start();
+    include("./partials/adminsidebar.php") ?>
+    <div id="add-body" class="addbody">
+
+        <div class="container">
+            <h1 class="view-title">PT Clients:</h1>
+            <input type="text" id="searchBar" onkeyup="myFunction()" placeholder="Search for Client names..">
+            <div id="tablediv">
+                <table id="ptMembershipsTable" class="view-table overflow-auto mh-10">
+                    <thead>
                         <tr>
-                          <th scope="col">Client ID</th>
+                            <th scope="col">ID</th>
                             <th scope="col">Client Name</th>
-                            <th scope="col">Client Phone</th>
-                            <th scope="col">Pt Package Name</th>
-                            <th scope="col">Sessions</th>
-                            <th scope="col">Action</th>
+                            <th scope="col">PT Package Name</th>
+                            <th scope="col">PT Package Number of Sessions</th>
+                            <th scope="col">Sessions Count</th>
+                            <th scope="col">Price</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th>1</th>
-                            <th>Mohamed</th>
-                            <th>0121084789</th>
-                            <th>20-sessions</th>
-                            <th>3 From 20</th>
-                            <th><button id="add-btn">Attend</button></th>
-                        </tr>
+                        <?php
+                        include_once "../Models/CoachesModel.php";
+
+                        $Coach = new Coach();
+                        $ptMembershipsData = $Coach->getptMembershipsForCoach($_SESSION['ID']);
+
+                        foreach ($ptMembershipsData as $pt) {
+                            echo "<tr>
+                               <td>" . $pt["ID"] . "</td>
+                               <td>" . $pt["clientFirstName"] . " " . $pt["clientLastName"] . "</td>
+                               <td>" . $pt["ptPackageName"] . "</td>
+                               <td>" . $pt["NumOfSessions"] . "</td>
+                               <td>" . $pt["SessionsCount"] . "</td>
+                               <td>" . $pt["Price"] . "</td>
+                               </td>
+                           </tr>";
+                        }
+
+                        ?>
                     </tbody>
-                    
-    </table>
-</div>
-</div>
+                </table>
+            </div>
+            <br>
+            <br>
+
+
+
+        </div>
+    </div>
+
+    <script>
+    function myFunction() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("searchBar");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("ptMembershipsTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+  </script>
+
 </body>
+
 </html>
