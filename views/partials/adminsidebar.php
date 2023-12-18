1dbb22b8bc897b0d7ce419986900f6bd53f17f65
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-12-18 21:31:57 +0200

    90% from employee authorities

--- views/partials/adminsidebar.php
+++ views/partials/adminsidebar.php
@@ -18,7 +18,7 @@ session_start();
 require "../Controllers/EmployeeController.php";
 $model = new Employee();
 $emp= new EmployeeController($model);
-$result=$emp->get_emp_auth(4);
+$result=$emp->get_emp_auth(3);
 foreach($result as $res){
     echo $res["Header"];
 }
@@ -30,15 +30,22 @@ foreach($result as $res){
         <span class="logo-name" style="font-weight:300">Admin</span>
     </div>
     <ul class="nav-links">
-        <li>
-            <a href="../views/admindashboard.php">
+        <?php 
+        foreach($result as $res){
+          if ($res["Header"]==='Dashboard'){ 
+            echo "  <li>
+            <a href=".$res["LinkAddress"].">
                 <i class='bx bxs-grid-alt'></i>
-                <span class="link-name">Dashboard</span>
+                <span class='link-name'>".$res["FriendlyName"]."</span>
             </a>
-            <ul class="sub-menu blank">
-                <li><a class="link-name" href="../views/admindashboard.php">Dashboard</a></li>
+            <ul class='sub-menu blank'>
+                <li><a class='link-name' href='../views/admindashboard.php'>".$res["LinkAddress"]."</a></li>
             </ul>
-        </li>
+        </li>";
+          }
+        }
+        ?>
+      
         <li>
             <div class="icon-link">
                 <a href="#">
@@ -49,11 +56,14 @@ foreach($result as $res){
             </div>
             <ul class="sub-menu">
                 <li><a class="link-name" href="../views/clientdetails.php">Clients</a></li>
-                <li><a href="../views/addclient.php">Add</a></li>
-                <!-- <li><a href="../views/editclient.php">Edit</a></li> -->
-                <li><a href="../views/allclients.php">Details</a></li>
-                <li><a href="../views/clientdetails.php">Memberships</a></li>
-                <li><a href="../views/clientin.php">Check in </a></li>
+                <?php
+                foreach($result as $res){
+                    if ($res["Header"]==='Clients'){
+                        echo "<li><a href=".$res["LinkAddress"].">".$res["FriendlyName"]."</a></li>";
+                    } 
+                }
+                ?>
+                
             </ul>
         </li>
 
@@ -68,12 +78,13 @@ foreach($result as $res){
             </div>
             <ul class="sub-menu">
                 <li><a class="link-name" href="../views/coachesadmin.php">Coaches</a></li>
-                <li><a href="../views/coachesadmin.php"> View Coaches </a></li>
-                <li><a href="../views/ptclients.php">View Pt Clients </a></li>
-                <li><a href="../views/myptclients.php">My Pt Clients </a></li>
-                <li><a href="../views/admin-classes.php">View Classes</a></li>
-                <li><a href="../views/addClass.php">Add Classes</a></li>
-                <li><a href="../views/myclasses.php">My Classes</a></li>
+                <?php
+                foreach($result as $res){
+                    if ($res["Header"]==='Coaches'){
+                        echo "<li><a href=".$res["LinkAddress"].">".$res["FriendlyName"]."</a></li>";
+                    } 
+                }
+                ?>
             </ul>
         </li>
 
@@ -87,9 +98,13 @@ foreach($result as $res){
             </div>
             <ul class="sub-menu">
                 <li><a class="link-name" href="#">HR </a></li>
-                <li><a href="../views/employeesadmin.php">View Employees</a></li>
-                <li><a href="../views/addEmployee.php">Add Employees</a></li>
-                <li><a href="../views/attendance.php">Attendance</a></li>
+                <?php
+                foreach($result as $res){
+                    if ($res["Header"]==='HR'){
+                        echo "<li><a href=".$res["LinkAddress"].">".$res["FriendlyName"]."</a></li>";
+                    } 
+                }
+                ?>
             </ul>
         </li>
         <li>
@@ -102,9 +117,14 @@ foreach($result as $res){
             </div>
             <ul class="sub-menu">
                 <li><a class="link-name" href="#">Admin </a></li>
-                <!-- <li><a href="../views/addadmin.php">Add Admin</a></li>
-                <li><a href="../views/removeadmin.php">Remove Admin</a></li> -->
-                <li><a href="../views/salesreport.php">Sales Report</a></li>
+               
+                <?php
+                foreach($result as $res){
+                    if ($res["Header"]==='Admin'){
+                        echo "<li><a href=".$res["LinkAddress"].">".$res["FriendlyName"]."</a></li>";
+                    } 
+                }
+                ?>
             </ul>
         </li>
         <li>
@@ -117,15 +137,20 @@ foreach($result as $res){
             </div>
             <ul class="sub-menu">
                 <li><a class="link-name" href="#">Packages</a></li>
-                <li><a href="../views/addPackageadmin.php">Add Package </a></li>
-                <li><a href="../views/viewpackagesadmin.php">View Packages </a></li>
-                <li><a href="../views/addPTpackage.php">Add PT Package </a></li>
-                <li><a href="../views/viewPTpackage.php">View PT Packages </a></li>
+                <?php
+                foreach($result as $res){
+                    if ($res["Header"]==='Packages'){
+                        echo "<li><a href=".$res["LinkAddress"].">".$res["FriendlyName"]."</a></li>";
+                    } 
+                }
+                ?>
 
             </ul>
         </li>
 
         <li>
+
+        
             <div class="icon-link">
                 <a href="../views/requests.php">
                     <i class='bx bx-info-circle' style="font-size:24px;" ></i>
@@ -133,7 +158,13 @@ foreach($result as $res){
                 </a>
             </div>
             <ul class="sub-menu blank">
-                <li><a class="link-name" href="../views/requests.php">Requests</a></li>
+            <?php
+                foreach($result as $res){
+                    if ($res["Header"]==='Requests'){
+                        echo "<li><a href=".$res["LinkAddress"].">".$res["FriendlyName"]."</a></li>";
+                    } 
+                }
+                ?>
             </ul>
         </li>
 
