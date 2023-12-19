6012624f6d113aef0a97e18f81176ebf4a2a04c9
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-12-20 00:26:42 +0200

    attendnace model path

--- Controllers/attendance.php
+++ Controllers/attendance.php
@@ -1,21 +1,24 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    include_once "../Models/EmployeeModel.php";
+
+    include_once "../Models/AttendanceModel.php";
 
     $json_data = file_get_contents("php://input");
     $decoded_data = json_decode($json_data, true);
     $date = $decoded_data["date"];
 
+
     foreach ($decoded_data as $key => $value) {
-        // $x=$value;
+        $x=$value;
         if ($key !== "date") {
 
             $key = str_replace('emp', '', $key);
 
-            $Attendance = new Attendance();
-            $Attendance->attendanceforemp($key, $value, $date);
+            $attendance = new Attendance();
+            $attendance->attendanceforemp($key, $value, $date);
         }
     }
     echo json_encode(["status" => "success"]);
+    // echo json_encode(["status" => "success"]);
 }
 ?>
